1c46ea52304c893b134fdf0d04b2dfda0e47f893
mapaaa <maria.pandele33@gmail.com>
2017-05-16 11:06:10 +0300

    Updated users

--- topcoder.py
+++ topcoder.py
@@ -1,6 +1,14 @@
 from base import *
 from typing import List, Tuple
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 
 def get_ranking(url: str) -> List[Tuple[str, int]]:
+    html = get_html(url)
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    table = soup.find_all('table', class_='mainContent')
+    print(table)
     return []
